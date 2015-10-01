8bd679e443810feadeb6cf4cdd325d6c31c6f578
Phuc <phuc.t.vo.bkdn@gmail.com>
2015-10-01 10:32:36 +0700

    Add password reset & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # get 'sessions/new'
-
-  # get 'users/new'
 
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
@@ -16,6 +13,7 @@ Rails.application.routes.draw do
   # get 'welcome' => 'users#index'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
