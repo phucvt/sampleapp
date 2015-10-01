7275ed91f571ca4c4aac44631d627d6e74ecadc2
Phuc <phuc.t.vo.bkdn@gmail.com>
2015-10-01 15:04:37 +0700

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,18 @@ User.create!(name:  "Example User",
              activated_at: Time.zone.now)
 
 30.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
+      name  = Faker::Name.name
+      email = "example-#{n+1}@railstutorial.org"
+      password = "password"
+      User.create!(name:  name,
               email: email,
               password:              password,
               password_confirmation: password,
               activated: true,
               activated_at: Time.zone.now)
+end
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
